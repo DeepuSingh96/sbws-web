4e3d68d2cdf0defb560d2e05e73e249310f6d12e
DESKTOP-QLK0E6T\asus <sachin.upadhyay-external@tcs.com>
2020-07-17 13:38:50 +0530

    Added Create Request
    Added Create Request functionality

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { RegisterUserComponent } from './components/pages/register-user/register-user.component';
 import { DashboardComponent } from './components/pages/dashboard/dashboard.component';
+import { AddUserComponent } from './components/dialog/add-user/add-user.component';
 import { ErrorComponent } from './components/error/error.component';
 import { RouteGuardService } from './service/routeGuard/route-guard.service'
 
@@ -11,6 +12,7 @@ const routes: Routes = [
   {path:'', component:RegisterUserComponent},
   {path:'login', component:RegisterUserComponent},
   {path:'dashboard/:username', component: DashboardComponent,canActivate:[RouteGuardService]},
+  {path:'createsbwsrequest', component:AddUserComponent},
   {path:'**', component:ErrorComponent }
 ];
 
