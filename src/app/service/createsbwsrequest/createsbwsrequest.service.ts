17271beed785c122c01b4e2dbbad473c2e68cb08
Preetha Vijayan <preetha.vijayan@tcs.com>
2020-08-12 13:27:00 +0530

    create request response changes

--- src/app/service/createsbwsrequest/createsbwsrequest.service.ts
+++ src/app/service/createsbwsrequest/createsbwsrequest.service.ts
@@ -12,7 +12,8 @@ export class CreatesbwsrequestService {
     const headers = { 'content-type': 'application/json'}  
     const body=JSON.stringify(request);
     const username=sessionStorage.getItem('authenticaterUser')
-    return this.http.post(`http://localhost:8181/dashboard/${username}/usersDetails`,body,{'headers':headers});
+    return this.http.post<any>(`http://localhost:8181/dashboard/${username}/usersDetails`,body,{'headers':headers,responseType: 'text'as 'json'});
+  
   }
   public updaterequest(username,employeeNo,upData) {
     const headers = { 'content-type': 'application/json'}  
