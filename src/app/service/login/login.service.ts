7204881e3be17b5e43af272793d35aa61edd28c0
Preetha Vijayan <preetha.vijayan@tcs.com>
2020-08-12 13:02:09 +0530

    security changes

--- src/app/service/login/login.service.ts
+++ src/app/service/login/login.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
+import {map} from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -11,7 +12,40 @@ export class LoginService {
   public SignIn(username:string,password:string)
   {
     let employeeNo = username;
-    const headers = new HttpHeaders({AUTHORIZATION: 'Basic '+btoa(username+":"+password)});
-    return this.http.post("http://localhost:8181/login",{employeeNo:employeeNo,password:password},{responseType:'text' as 'json'})
+
+    let header = new HttpHeaders({Authorization:'Basic '+btoa(username+":"+password)});
+    //type of header
+    return this.http.post("http://localhost:8181/login",{employeeNo:employeeNo,password:password},{headers : header,responseType:'text' as 'json'})
+
+  }
+  getAuthenticatedUser() {
+    return sessionStorage.getItem('authenticaterUser')
+  }
+
+  getAuthenticatedToken() {
+    if(this.getAuthenticatedUser())
+      return sessionStorage.getItem('Token')
+  }
+
+   executeJWTAuthenticationService(username:string,password:string)
+  {
+    let employeeNo = username;
+
+    //let header = new HttpHeaders({Authorization:'Basic '+btoa(username+":"+password)});
+    //type of header
+    console.log('in jwt login service')
+    let headers = new HttpHeaders();
+    let header = headers.set('skip', 'ABCD');
+    return this.http.post<any>("http://localhost:8181/login",{employeeNo:employeeNo,password:password},{headers:header})
+    .pipe(
+      map(
+        data=>{
+          sessionStorage.setItem('authenticaterUser',username);
+          sessionStorage.setItem('Token',`Bearer ${data.jwt}`);
+          return data;
+        }
+      )
+    )
+
   }
 }
